0b7f732de68989e65221a932702fa5d8fb1d05d0
Paul Kenny <paul.s.kenny@gmail.com>
2018-04-12 09:03:33 +0100

    Change transaction/account buttons, refactor, add button methods

--- Yapfa/MainPage.xaml.cs
+++ Yapfa/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -15,36 +16,31 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace Yapfa
 {
-    public class PieSegment
-    {
-        public string Name { get; set; }
-        public int Amount { get; set; }
-    }
-
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        ObservableCollection<Account> accountsList;
-        ObservableCollection<Transaction> transactionsList;
-
+        ObservableCollection<Account> accountsList = new ObservableCollection<Account>();
+        ObservableCollection<Transaction> transactionsList = new ObservableCollection<Transaction>();
 
         public MainPage()
         {
             this.InitializeComponent();
             this.Loaded += MainPage_Loaded;
+
+            transactionsList.CollectionChanged += TransactionListChange;
         }
 
         void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            // Load categories, payees, accounts and transactions
-            LoadData();
+            // TODO load settings
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            Windows.Storage.StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            DisplayStartupDialog();
 
             // Set item sources
             AccountTable.ItemsSource = accountsList;
@@ -53,16 +49,36 @@ namespace Yapfa
             UpdateChart();
         }
 
+        private async void DisplayStartupDialog()
+        {
+            ContentDialog subscribeDialog = new ContentDialog
+            {
+                Title = "Hello",
+                Content = "What do you want to do?",
+                CloseButtonText = "Empty",
+                PrimaryButtonText = "Load File",
+                SecondaryButtonText = "Load Sample Data",
+                DefaultButton = ContentDialogButton.Secondary
+            };
+
+            ContentDialogResult result = await subscribeDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                // TODO open dialog, open serialised data
+            }
+            else if (result == ContentDialogResult.Secondary)
+            {
+                // Load categories, payees, accounts and transactions
+                LoadSampleData();
+            }
+        }
+
         /// <summary>
         /// Load accounts, categories, payees and transactions
         /// </summary>
-        private void LoadData()
+        private void LoadSampleData()
         {
-            accountsList = new ObservableCollection<Account>();
-            transactionsList = new ObservableCollection<Transaction>();
-
-            // Load test data if in debug mode
-#if DEBUG
             // Make test accounts
             Account account1 = new Account()
             {
@@ -134,9 +150,6 @@ namespace Yapfa
             AddTransaction(tr3);
             AddTransaction(tr4);
             AddTransaction(tr5);
-#else
-            // Load real data, previous instance if available
-#endif
         }
 
         void AddTransaction(Transaction transaction)
@@ -145,6 +158,7 @@ namespace Yapfa
 
             for (var i = 0; i < accountsList.Count; i++)
             {
+                // Add transaction amount to account with same name
                 if (accountsList[i].Name == transaction.Account)
                 {
                     accountsList[i].Balance += transaction.Amount;
@@ -156,17 +170,62 @@ namespace Yapfa
         {
             // TODO Generate category totals for income/expenditure from transactions
             List<PieSegment> financialStuffList = new List<PieSegment>();
-            
+
             foreach (Transaction transaction in transactionsList)
             {
                 if (transaction.Amount < 0 && transaction.Category.Length > 0)
                 {
                     // TODO check if category is already present, add to value if true
-                    financialStuffList.Add(new PieSegment() { Name = transaction.Category, Amount = (int) transaction.Amount });
+                    financialStuffList.Add(new PieSegment() { Name = transaction.Category, Amount = (int)transaction.Amount });
                 }
             }
 
             (PieChart.Series[0] as PieSeries).ItemsSource = financialStuffList;
+        }
+
+        private void TransactionListChange(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            UpdateChart();
+        }
+
+        private void AddAccount_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            AddAccountFlyout.ShowAt(button);
+        }
+
+        private void RemoveAccount_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            if (AccountTable.SelectedItem != null)
+            {
+                RemoveAccountFlyout.ShowAt(button);
+            }
+        }
+
+        private void AddTransaction_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            AddTransactionFlyout.ShowAt(button);
+        }
+
+        private void RemoveTransaction_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            if (TransactionsTable.SelectedItem != null)
+            {
+                RemoveTransactionFlyout.ShowAt(button);
+            }
+        }
+
+        private class PieSegment
+        {
+            public string Name { get; set; }
+            public int Amount { get; set; }
         }
     }
 }
